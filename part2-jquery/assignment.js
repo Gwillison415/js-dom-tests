75f4d55f84b2ab881e1339085f0f3c483f2d9c04
Grant Willison <emailgdw@gmail.com>
2017-05-05 13:01:14 -0700

    Finished JS dom part 2 jQuery

--- part2-jquery/assignment.js
+++ part2-jquery/assignment.js
@@ -4,7 +4,9 @@
 //
 // The function will return true if the element has the specified className CSS
 // class applied.
-
+function hasClass(element, className) {
+  return $(element).hasClass(className);
+}
 
 
 // Define a function named toggleVisible that takes in one argument.
@@ -13,16 +15,26 @@
 // The function will add the 'visible' class to the div if it does not currently
 // have it. It removes the class if it already exists.
 
+function toggleVisible(div) {
+    return $(div).toggleClass("visible");
 
+}
 
+// .toggle( display )
+// display
+// Type: Boolean
+// Use true to show the element or false to hide it.
 // Define a function named hideConfidentialText that takes in one argument.
 //   article (<article> DOM element)
 // Assume the article contains at least one paragraph.
 //
 // The function will hide any child paragraphs of article if anywhere the text contains
 // "CONFIDENTIAL"
+function hideConfidentialText(article) {
+  $(article).children("p:contains('CONFIDENTIAL')").hide();
+}
 
-
+//$( "div:contains('John')" ).css( "text-decoration", "underline" );
 
 // Define a function named checkAll that takes in one argument
 //   checkbox (<input type="checkbox"> DOM element).
@@ -30,7 +42,11 @@
 // The function will mark all sibling checkboxes, of the input checkbox, as "checked" if the input checkbox
 // is marked as "checked".
 
-
+function checkAll(checkbox) {
+  if ($(checkbox).is(":checked")) {
+   return $(checkbox).siblings().prop('checked', true);
+  }
+}
 
 // Define a function named updateTodoList that takes one argument.
 //   todoList (<ul> DOM element)
@@ -41,6 +57,12 @@
 //     TIP: Applying a CSS class means adding on top of what's already there.
 //   * Make no change otherwise.
 
+function updateTodoList(todoList) {
+  $(todoList).children("li:contains('COMPLETED')").remove();
+  $(todoList).children("li:contains('URGENT')").addClass(  'important');
+
+  // attr('style', function(i,s) { return s + '!important;' });
+}
 
 
 // Define a function named createList that takes one argument.
@@ -70,7 +92,40 @@
 //      <li><a href="https://github.com">GitHub</a></li>
 //      <li><a href="https://www.galvanize.com">Galvanize</a></li>
 //    </ul>
+// var object = ({
+//      'TITLE': 'URL',
+//      'TITLE': 'URL',
+//      'TITLE': 'URL',
+//    });
+function createList(sites) {
+  let returnedUl = $("<ul>");
 
+  if (sites.length !==0) {
+    for (let site in sites) {
+      //TEST TO SEE IF ALSO WORKS
+      // var a = $('<a>').attr('href', sites[site]).attr('text', site);
+      // let li = $('<li>').attr('text', a);
+      var a = $('<a>').attr('href', sites[site]).text(site);
+      let li = $("<li>");
+
+      a.appendTo(li);
+
+
+      //alt implementation -DOES NOT WORK
+      //$('<a>').attr('href', sites[site]).text(site).appendTo(li);
+
+
+      // let li = $('<li>').attr('html', $('<a>')).attr('href', sites[site]).attr('innerText', site);
+      li.appendTo(returnedUl);
+    }
+  }
+    //console.log('a', a, 'ul list', returnedUl);
+return returnedUl[0];
+}
+
+
+// clipboard
+// $("#header ul").append('<li><a href="/user/messages"><span class="tab">Message Center</span></a></li>');
 
 
 // Write a function named extractQuote that takes in one argument.
@@ -97,7 +152,21 @@
 // TIP: Assume that if there's an opening double quote, there's a closing
 // double quote as well.
 
+function extractQuote(articleTag) {
+  let blockQ = $('<blockquote>');
+  if ($(articleTag).children("p:contains(\")").text()) {
+    var text = $(articleTag).children("p:contains(\")").text();
+    // console.log(text, 'text.text', text.text);
+    let quotedText = text.slice(text.indexOf('"'), text.lastIndexOf('"') + 1);
 
+    $(articleTag).children('p:contains(\")').remove();
+    blockQ.text(quotedText);
+    blockQ.appendTo(articleTag);
+    //console.log(articleTag);
+  }
+
+  return articleTag;
+}
 
 // Define a function named createTable that takes one argument.
 //   data (array of arrays)
@@ -146,3 +215,146 @@
 //
 // TIP: Assume that data array has at least three elements.
 // TIP: Assume that the elements of the data array are equal in length.
+function createTable(data) {
+   let tableData = data.slice();
+   let header = tableData.shift();
+   let footer = tableData.pop();
+
+   var table = $('<table>');
+   var thead = $('<thead>');
+   $(table).append(thead);
+
+
+   //$(thead).append(tr);
+
+
+  
+   let trHead = $('<tr>')
+
+  //  console.log(header, tableData, footer);
+  //  thead.append(tr);
+
+   trHead.appendTo($(thead));
+   //set up table head
+
+   for (var i = 0; i < header.length; i++) {
+     var tr = $('<tr>');
+    //  $(trHead).append($(th));
+    //  th.text(header[i]);
+    //  $(tr).appendTo(th);
+
+     let th = $('<th>')
+     th.appendTo(trHead);
+     th.text(header[i]);
+   }
+   let tbody = $('<tbody>');
+   $(table).append(tbody);
+   //set up table body
+   for (var i = 0; i < tableData.length; i++) {
+     let tr = $('<tr>');
+
+     $(tbody).append($(tr));
+
+     console.log(tableData[i]);
+     for (var j = 0; j < tableData[i].length; j++) {
+       var td = $('<td>');
+       td.text(tableData[i][j]);
+       $(td).appendTo(tr);
+       //console.log(tableData[i][ii], "happens");
+
+     }
+   }
+
+  //  $.each(tableData, (idx, elem) => {
+  //     tr.append(td);
+  //     td.text(elem);
+  //  })
+
+   //set up footer
+   let tfoot = $('<tfoot>')
+   table.append(tfoot);
+   let trFoot = $('<tr>')
+   trFoot.appendTo($(tfoot));
+   console.log('footer', footer);
+   for (var i = 0; i < footer.length; i++) {
+     let td = $('<td>')
+     td.appendTo(trFoot);
+     td.text(footer[i]);
+
+   }
+
+
+   console.log(table[0]);
+   return table[0];
+}
+
+// function createTable(data) {
+//    let tableData = data.slice();
+//    let header = tableData.shift();
+//    let footer = tableData.pop();
+//
+//    var table = $('<table>');
+//    var thead = $('<thead>');
+//    $(table).append(thead);
+//
+//
+//    $(thead).append(tr);
+//
+//
+//    let th = $('<th>');
+//
+//
+//   //  console.log(header, tableData, footer);
+//   //  thead.append(tr);
+//
+//   th.appendTo($(thead));
+//    //set up table head
+//    for (var i = 0; i < header.length; i++) {
+//      var tr = $('<tr>');
+//
+//      tr.text(header[i]);
+//      $(tr).appendTo(th);
+//
+//    }
+//
+//
+//    let tbody = $('<tbody>');
+//    $(table).append(tbody);
+//
+//    //set up table body
+//    for (var i = 0; i < tableData.length; i++) {
+//      var tr = $('<tr>');
+//
+//      $(tbody).append($(tr));
+//
+//      console.log(tableData[i]);
+//      for (var j = 0; j < tableData[i].length; j++) {
+//        var td = $('<td>');
+//        td.text(tableData[i][j]);
+//        $(td).appendTo(tr);
+//        //console.log(tableData[i][ii], "happens");
+//      }
+//    }
+//    tr.appendTo($(tbody));
+//   //  $.each(tableData, (idx, elem) => {
+//   //     tr.append(td);
+//   //     td.text(elem);
+//   //  })
+//
+//    //set up footer
+//    let tfoot = $('<tfoot>')
+//    table.append(tfoot);
+//    let trfoot = $('<tr>')
+//    tr.appendTo($(tfoot));
+//    console.log('footer', footer);
+//    for (var i = 0; i < footer.length; i++) {
+//      let td = $('<td>')
+//      td.appendTo(trfoot);
+//      td.text(footer[i]);
+//
+//    }
+//
+//
+//    console.log(table[0]);
+//    return table[0];
+// }
